6ad902294f76e6c950f1b563f10758fd932b6633
shivanXI <shivanstar7@gmail.com>
2017-07-08 00:45:00 +0530

    Added signaling node js server using socket.io for real time data transfer

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res){
 	fileServer.serve(req, res);
 }).listen(8080);
+
+var io = socketIO.listen(app);
+io.sockets.on('connection', function(socket) {
+
+	// convenience function to log server messages to client
+	function log() {
+		var array = ['Message from server:'];
+		array.push.apply(aray, arguments);
+		socket.emit('log', array);
+	}
+
+	
+})
