4ce38f30456aa05bd4206e1f8ab9a26a4bb40300
shivanXI <shivanstar7@gmail.com>
2017-07-08 01:32:42 +0530

    Added final whole room functionality seetings

---
+++ js/main_final.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var isChannelReady = false;
+var isInitiator = false;
+var isStarted = false;
+var localStream;
+var pc;
+var remoteStream;
+var turnReady;
+
+var pcConfig = {
+  'iceServers': [{
+    'urls': 'stun:stun.l.google.com:19302'
+  }]
+};
