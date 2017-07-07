db22713370e27b072b56df79a7ab763df1e52033
shivanXI <shivanstar7@gmail.com>
2017-07-08 01:19:17 +0530

    Added main_room.js for control of no of rooms active for video call-01

--- js/main_room.js
+++ js/main_room.js
@@ -10,3 +10,20 @@ if (room !== "") {
 	console.log('Message from client: Asking to join room ' + room);
 	socket.emit('create or join', room);
 }
+
+socket.on('created', function(room, clientId) {
+	isInitiator = true;
+});
+
+socket.on('joined', function(room, clientId) {
+	isInitiator = false;
+});
+
+socket.on('full', function(room) {
+  console.log('Message from client: Room ' + room + ' is full :^(');
+});
+
+socket.on('ipaddr', function(ipaddr) {
+  console.log('Message from client: Server IP address is ' + ipaddr);
+});
+
