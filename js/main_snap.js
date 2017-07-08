651b947526ad0de0a7fc3b6f85f284e96c303edd
shivanXI <shivanstar7@gmail.com>
2017-07-08 09:51:46 +0530

    grabbing video from cam-01

--- js/main_snap.js
+++ js/main_snap.js
@@ -41,3 +41,30 @@ socket.on('created', function(room, clientId) {
   isInitiator = true;
   grabWebCamVideo();
 });
+
+socket.on('joined', function(room, clientId) {
+  console.log('This peer has joined room', room, 'with client ID', clientId);
+  isInitiator = false;
+  createPeerConnection(isInitiator, configuration);
+  grabWebCamVideo();
+});
+
+socket.on('full', function(room) {
+  alert('Room ' + room + ' is full. We will create a new room for you.');
+  window.location.hash = '';
+  window.location.reload();
+});
+
+socket.on('ready', function() {
+  console.log('Socket is ready');
+  createPeerConnection(isInitiator, configuration);
+});
+
+socket.on('log', function(array) {
+  console.log.apply(console, array);
+});
+
+socket.on('message', function(message) {
+  console.log('Client received message:', message);
+  signalingMessageCallback(message);
+});
