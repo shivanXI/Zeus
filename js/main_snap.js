35766e7744a5b6fa09a7b8b4475e6ab7a258d7e3
shivanXI <shivanstar7@gmail.com>
2017-07-08 09:41:04 +0530

    Added snap transfer functionality

---
+++ js/main_snap.js
@@ -0,0 +1,10 @@
+var configuration = null;
+
+// var roomURL = document.getElementById('url');
+var video = document.querySelector('video');
+var photo = document.getElementById('photo');
+var photoContext = photo.getContext('2d');
+var trail = document.getElementById('trail');
+var snapBtn = document.getElementById('snap');
+var sendBtn = document.getElementById('send');
+var snapAndSendBtn = document.getElementById('snapAndSend');
