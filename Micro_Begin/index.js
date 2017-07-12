d90ceaf8d170c120273a47c0843178e7ec0fe608
shivanXI <shivanstar7@gmail.com>
2017-07-12 20:48:27 +0530

    Adding microservice for zeus-01 from scratch using redis and hydra

---
+++ Micro_Begin/index.js
@@ -0,0 +1,12 @@
+var hydraExpress = require ('fwsp-hydra-express');
+var config = require('./config.json');
+//var app = express()
+
+
+//app.get('/', function (req, res) {
+//	res.send("Hello Sexy Bitch !! :P")
+//})
+
+//app.listen(3000, function () {
+//	console.log('App listening on port to dock it baby on 3000 !')
+//})
