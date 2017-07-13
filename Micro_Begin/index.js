c87b07afbe5cd8ec415458fde8094479c8cef0b3
shivanXI <shivanstar7@gmail.com>
2017-07-13 20:41:36 +0530

    Added multiple instances with hydra-express

--- Micro_Begin/index.js
+++ Micro_Begin/index.js
@@ -8,7 +8,8 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {  
   console.log('App is listening where are you port 3000! :P')
 })*/
-var hydraExpress = require('fwsp-hydra-express');  
+var hydraExpress = require('fwsp-hydra-express');
+var hydra = hydraExpress.getHydra();
 var config = require('./config.json');
 
 function onRegisterRoutes() {  
@@ -16,7 +17,9 @@ function onRegisterRoutes() {
   var api = express.Router();
 
   api.get('/', function(req, res) {
-    res.send('WhatsUp Dude!!');
+    res.send({
+    	msg: 'WhatsUp Dude!! ${hydra.getServiceName()} - ${hydra.getInstanceID()}'
+    });
   });
   hydraExpress.registerRoutes({
     '': api
