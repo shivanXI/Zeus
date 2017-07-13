74890354354bcc4611528957b11d8eef2b8a0a67
shivanXI <shivanstar7@gmail.com>
2017-07-13 19:45:29 +0530

    Adding microservices from scratch-01

--- Micro_Begin/index.js
+++ Micro_Begin/index.js
@@ -1,28 +1,10 @@
+var express = require('express')  
+var app = express()
 
-//var app = express()
-var hydraExpress = require('fwsp-hydra-express');  
-var config = require('config.json');
+app.get('/', function (req, res) {  
+  res.send('WhatsUp Dude!!')
+})
 
-//app.get('/', function (req, res) {
-//	res.send("Hello Sexy Bitch !! :P")
-//})
-
-//app.listen(3000, function () {
-//	console.log('App listening on port to dock it baby on 3000 !')
-//})
-
-
-
-function onRegisterRoutes() {  
-  var express = hydraExpress.getExpress();
-  var api = express.Router();
-
-  api.get('/', function(req, res) {
-    res.send('Hello Sexy Bitch !! :P');
-  });
-  hydraExpress.registerRoutes({
-    '': api
-  });
-}
-
-hydraExpress.init(config, onRegisterRoutes); 
+app.listen(3000, function () {  
+  console.log('App is listening where are you port 3000! :P')
+})
