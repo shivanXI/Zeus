85731ad15d4e4a3c51f24f62c57a89848444ae30
shivanXI <shivanstar7@gmail.com>
2017-07-13 21:44:03 +0530

    Added multiple services messaging-04

--- Micro_Begin/friend.js
+++ Micro_Begin/friend.js
@@ -7,14 +7,22 @@ config.hydra.serviceName = 'friend';
 hydraExpress.init(config, () => {})
 	.then((serviceInfo) => {
 		console.log('serviceInfo', serviceInfo);
+
+		hydra.on('message', (message) => {
+			console.log('message reply', message);
+		});
+
 		let message = hydra.createUMFMessage({
 			to: 'hello:[get]/',
 			from: 'friend:/',
 			body: {}
 		});
-		return hydra.makeAPIRequest(message)
-			.than((response) => {
-				console.log('response', response);
-			});
+
+		hydra.sendMessage(message);
+
+		//return hydra.makeAPIRequest(message)
+		//	.than((response) => {
+		//		console.log('response', response);
+		//	});
 	})
 	 .catch(err => console.log('err', err));
